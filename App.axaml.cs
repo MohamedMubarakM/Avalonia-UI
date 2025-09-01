8f1050f9b7a474fe55614b77a3c1bd0596dbf58c
MohamedMubarakM <44994337+MohamedMubarakM@users.noreply.github.com>
2025-09-01 18:40:56 +0530

    Initialize + first push

---
+++ App.axaml.cs
@@ -0,0 +1,20 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using Avalonia1.Views;
+
+namespace Avalonia1
+{
+    public partial class App : Application
+    {
+        public override void Initialize() => AvaloniaXamlLoader.Load(this);
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+                desktop.MainWindow = new MainWindow();
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ ViewLocator.cs
@@ -0,0 +1,26 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+
+namespace Avalonia1
+{
+    public sealed class ViewLocator : IDataTemplate
+    {
+        public bool SupportsRecycling => true;
+
+        public Control? Build(object? data)
+        {
+            if (data is null) return new TextBlock { Text = "null view model" };
+
+            var name = data.GetType().FullName?.Replace("ViewModel", "View");
+            if (string.IsNullOrWhiteSpace(name)) return new TextBlock { Text = "invalid view model" };
+
+            var type = Type.GetType(name);
+            if (type is null) return new TextBlock { Text = $"Not found: {name}" };
+
+            return (Control)Activator.CreateInstance(type)!;
+        }
+
+        public bool Match(object? data) => data is not null;
+    }
+}
